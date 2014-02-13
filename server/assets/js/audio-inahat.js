d7006198df64894820f29a6a1a38fcf3b1500c30
miketahani <m.tahani@gmail.com>
2014-02-13 02:54:32 -0800

    ugh forgot to divide by index by 4 when mapping 256b audio buffer to 1024b texture

--- server/assets/js/audio-inahat.js
+++ server/assets/js/audio-inahat.js
@@ -9,6 +9,7 @@ var streamOptsDefaults = {
   outputChannels: 1
 };
 
+
 var processors = {
 
   defaultProcessor: function(e) {
@@ -38,13 +39,14 @@ var processors = {
 
     var buffer = e.inputBuffer.getChannelData(0);
     var b = 256 * 4;
-    // var data   = new Uint8Array(b);  // r,g,b,a for each data point
     var data   = new Float32Array(b);
+    
     for (var i = 0; i < b; i+=4) {
-      data[i] = (buffer[i] + 1) / 2;
+      var datum = (buffer[i/4] + 1) / 2;
+      data[i] = datum;
       data[i+1] = 0;
       data[i+2] = 0;
-      data[i+3] = 0; 
+      data[i+3] = 1; 
     }
 
     updateAudioTexture(data);
@@ -84,20 +86,23 @@ function setupAudioStream(streamOpts, audioDataProcessor) {
 function initAudioTexture() {
   audioTexture = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, audioTexture);
-  // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-  // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-  // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+
+  // textureCoordAttribute = gl.getAttribLocation(currentProgram, "aTextureCoord");
+  // gl.enableVertexAttribArray(textureCoordAttribute);
+
+  gl.bindTexture(gl.TEXTURE_2D, null);
 }
 
 function updateAudioTexture(data) {
   gl.bindTexture(gl.TEXTURE_2D, audioTexture);
   gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 16, 16, 0, gl.RGBA, gl.FLOAT, data); // XXX
+  // gl.uniform1i(gl.getUniformLocation(currentProgram, "tex"), 11);
 }
 
 
